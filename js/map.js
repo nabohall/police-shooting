1899819f3d1777e9ecfb71c3d62667c1aa309b71
Nick Bohall <nabohall1224@icloud.com>
2015-10-19 20:50:40 -0700

    Finished website

--- js/map.js
+++ js/map.js
@@ -2,7 +2,7 @@
 var drawMap = function() {
 
   // Create map and set view
-  var map = L.map('container').setView([41.505, -100.09], 4);
+  var map = L.map('map_area').setView([41.505, -100.09], 5);
 
   // Create a tile layer variable using the appropriate url
   var tileURL = 'https://api.mapbox.com/v4/mapbox.outdoors/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibmFib2hhbGwiLCJhIjoiY2lmdnVjdzUwMjloZXR4a3IydzFvb2ZqZCJ9.UPAiSQN4Fh4nwr9rx_BI5w';
@@ -31,99 +31,126 @@ var getData = function(map) {
   });
 
   // When your request is successful, call your customBuild function
-
-console.log("Finished AJAX")
-  customBuild1(map);
-
 }
 
-
+//Creates the map and table after a successful AJAX request
 var customBuild = function(map, data) {
-	var layers = [];
-	var armedCount = {};
-	var unarmedCount = {};
+	var layers = {}; // Key: Race, Value: Map Layer
+	var killCount = {};// Key: armed/unarmed, Value: count
+	var hitCount = {};// Key: armed/unarmed, Value: count
+
 
 	data.forEach(function(d) {
-		var race = d["Race"];
-		var latitude = d["lat"];
-		var longitude = d["lng"];
-		var name = d["Victim Name"];
-		var age = d["Victim's Age"];
-		var summary = d["Summary"];
-		var link = d["Source Link"];
+		makeLayers(d, layers);
+
+		var kill = d["Hit or Killed?"];
 		var armed = d["Armed or Unarmed?"];
 
-		if(race == null && jQuery.type(layers[race]) !== "string") {
-			race = "Unknown";
-		}
+		if (armed == undefined) {
+			armed = "Unknown";
+		};
 
-		//If the race is does not have a group layer
-		if(layers[race] == null) {
-			layers[race] = new L.LayerGroup([]);
-		} // Do something if there is no race specified.
 
-		//Create a circle based on whether they victim was armed or unarmed
-		if( armed == "Armed" ) {
-			var circle = new L.circleMarker([latitude, longitude], {
-			    color: 'red',
-			    fillColor: '#fb0303',
-			    fillOpacity: 0.5,
-			    radius: 5
-			});
-
-			var anchor = $('<a></a>').text("link");
-			anchor.attr('href', link);
-			var popupInfo = name + '-' + age + "<br>" + summary + "<br>";
-			var popup = $('<p></p>').text(popupInfo);
-			popup.append(anchor);
-			circle.bindPopup(popup);
-			console.log(popup);
-			circle.addTo(layers[race]);
+		if(kill == "Hit") {
+			if(hitCount[armed] == null){
+				hitCount[armed] = 0;
+			}
+			hitCount[armed]++;
 		} else {
-			var circle = new L.circleMarker([latitude, longitude], {
-			    color: 'blue',
-			    fillColor: '#328eff',
-			    fillOpacity: 0.5,
-			    radius: 5
-			});
-
-			var anchor = $('<a></a>').text("See Source");
-			anchor.attr('href', link);
-
-			var popupInfo = name + '-' + age + "<br>" + summary + "<br>";
-			var popup = $('<p></p>').text(popupInfo);
-			popup.append(anchor);
-			circle.bindPopup(popup);
-			circle.addTo(layers[race]);
+			if(killCount[armed] == null){
+				killCount[armed] = 0;
+			}
+			killCount[armed]++;
 		}
+
+		
 
 	});
 
-	layers["Unknown"].addTo(map);
-	layers["White"].addTo(map);
-	console.log(layers["Unknown"]);
-	console.log(jQuery.type(layers));
-	layers.forEach(function(d) {
-		console.log(d);
-	})
+	createTable(killCount, hitCount);
+
+	//Add all layers to the map
+	for (var key in layers) {
+		layers[key].addTo(map);
+	}
 	
-	console.log("I got pass the adding of layers.")
+	//A control form.
+	L.control.layers(null,layers).addTo(map);
 	
 };
-// Loop through your data and add the appropriate layers and points
-var customBuild1 = function(map) {
-	// Be sure to add each layer to the map
-	console.log("Making other circle.")
 
-	var circle = new L.circleMarker([47.6097, -122.3331], {
-    color: 'red',
-    fillColor: '#f03',
-    fillOpacity: 0.5,
-    radius: 5
-}).bindPopup('This is Seattle');
-	// Once layers are on the map, add a leaflet controller that shows/hides layers
-	var testLayer = new L.LayerGroup([circle]);
-  
+//Creates the table with armed/unarmed vs. kill/hit
+var createTable = function(killCount, hitCount) {
+	var table = $('<table></table>').addClass("table");
+	var tr = $('<tr></tr>').append($('<td></td>'));
+
+	for (key in killCount) {
+		tr.append($('<td></td>').text(key).addClass("header"));
+	}
+	table.append(tr);
+
+	var tr  = $('<tr></tr>').append($('<td></td>').text("Killed").addClass("header"));
+	for (key in killCount) {
+		tr.append($('<td></td>').text(killCount[key]));
+	}
+	table.append(tr);
+
+	var tr  = $('<tr></tr>').append($('<td></td>').text("Hit").addClass("header"));
+	for (key in hitCount) {
+		tr.append($('<td></td>').text(hitCount[key]));
+	}
+	table.append(tr);
+
+	$('#table_area').append(table);
+}
+
+//Creates the points on the map corresponding to shootings.
+var makeLayers = function(d, layers) {
+	var race = d["Race"];
+	var latitude = d["lat"];
+	var longitude = d["lng"];
+	var name = d["Victim Name"];
+	var age = d["Victim's Age"];
+	var summary = d["Summary"];
+	var link = d["Source Link"];
+	var armed = d["Armed or Unarmed?"];
+
+	//Cleanse data if race is not specified.
+	if(race == null && jQuery.type(layers[race]) !== "string") {
+		race = "Unknown";
+	}
+
+	//If the race does not have a group layer, make one
+	if(layers[race] == null) {
+		layers[race] = new L.LayerGroup([]);
+	}
+
+	//Create a circle based on whether they victim was armed or unarmed, and add it to the appropriate layer
+	if( armed == "Armed" ) {
+		var circle = new L.circleMarker([latitude, longitude], {
+		    color: 'red',
+		    fillColor: '#fb0303',
+		    fillOpacity: 0.5,
+		    radius: 6
+		});
+
+		var popupInfo = name + ' - ' + age + ' - ' + armed + "<br>" + summary + "<br>";
+		circle.bindPopup("<p>" + popupInfo + "<a href= + " + link + " target='_blank'> (See more) </a> </p>");
+
+		circle.addTo(layers[race]);
+	} else {
+		var circle = new L.circleMarker([latitude, longitude], {
+		    color: 'blue',
+		    fillColor: '#328eff',
+		    fillOpacity: 0.5,
+		    radius: 5
+		});
+
+		var popupInfo = name + ' - ' + age +' - ' + armed + "<br>" + summary + "<br>";
+		circle.bindPopup("<p>" + popupInfo + "<a href=" + link + " target='_blank'> (See more) </a> </p>");
+
+		circle.addTo(layers[race]);
+	}
 }
 
 
